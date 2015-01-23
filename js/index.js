3256b86ca9e6b679ae5f62722403770b3de65d50
bvenne <Bastian.Vennemann@googlemail.com>
2015-01-23 14:34:32 +0100

    login test

--- js/index.js
+++ js/index.js
@@ -16,6 +16,53 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        
+        
+        
+
+
+
+
+
+$("#loginForm").on("submit",function(e) {
+	alert("submit was called!");
+//disable the button so we can't resubmit while we wait
+$("#submitButton",this).attr("disabled","disabled");
+var u = $("#username", this).val();
+var p = $("#password", this).val();
+if(u != '' && p!= '') {
+$.post("http://www.desy.de/~bvenne/sicher/", {username:u,password:p}, function(res) {
+if(res == true) {
+//$.mobile.changePage("some.html");
+alert("GEHT!!!");
+} else {
+navigator.notification.alert("Your login failed", function() {});
+alert("LOGIN FALSCH!");
+}
+$("#submitButton").removeAttr("disabled");
+},"json");
+}
+return false;
+});
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
