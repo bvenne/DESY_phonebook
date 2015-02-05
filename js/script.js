8de69677cf76a57cde5d82841ff7106822a080a7
bvenne <Bastian.Vennemann@googlemail.com>
2015-02-05 09:04:19 +0100

    splashscreens

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
 			$('#desylogo').click(function (e) {
 				e.preventDefault();
 				count += 1;
-				if (count == 6) {
+				if (count == 7) {
 					$('#popup').bPopup({
 						content:'image',
 						contentContainer:'.popupcontent',
